1b2560211799f66472b56e971a99ad5f136bc7e3
Nadav Shor <nadavs@nadavs-air.home>
2022-03-26 20:21:18 +0300

    added a line to the loading msg

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ export default function Loading(){
     return (
         <div className="loading-div">
             <h4 className="loading-header">Fetching Flights From Around The World For You..</h4>
-            <h6 className="loading-header">This May Take A Momonet (free api..)</h6>
+            <h6 className="loading-header">This May Take A Moment (free api..)</h6>
             <FillingBottle color="#ED6C02" width="5rem" height="5rem"/>
         </div>
     )
